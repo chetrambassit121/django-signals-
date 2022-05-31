fcd9aa7a22d6807e4abdbf6eddf8ab6474f9ee7d
Chetram Bassit <chetrambassit121@gmail.com>
2022-05-31 13:12:47 -0400

    models and signals for order app

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from cars.models import Car
 # Create your models here.
+class Order(models.Model):
+    name = models.CharField(max_length=200)
+    cars = models.ManyToManyField(Car)
+    total = models.PositiveBigIntegerField(blank=True)
+    total_price = models.PositiveBigIntegerField(blank=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.name) 
