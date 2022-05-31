816be3efdf2a5bec1e4ac9c806c3c6751ca009e8
Chetram Bassit <chetrambassit121@gmail.com>
2022-05-31 13:43:26 -0400

    added post_save_create_or_update_sale method to orders.signals

--- orders/signals.py
+++ orders/signals.py
@@ -1,6 +1,7 @@
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
 from .models import Order 
+from sales.models import Sale
 
 
 @receiver(m2m_changed, sender=Order.cars.through)
@@ -19,3 +20,11 @@ def m2m_changed_cars_order(sender, instance, action, **kwargs):
         instance.total = total 
         instance.total_price = total_price 
         instance.save()
+
+
+@receiver(post_save, sender=Order)
+def post_save_create_or_update_sale(sender, instance, created, **kwargs):
+    obj, _ = Sale.objects.get_or_create(order=instance)
+    obj.amount = instance.total_price
+    obj.save()
+
