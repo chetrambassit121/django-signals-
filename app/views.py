5209181c2073b5decfa36fb938a35e4722a78976
Chetram Bassit <chetrambassit121@gmail.com>
2022-05-31 18:37:35 -0400

    created app view and index page for project

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.core.signals import request_finished
+from django.dispatch import receiver
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("<h1>Welcome to Chets Django Signals Basic</h1>")
+
+
+@receiver(request_finished)
+def my_callback(sender, **kwargs):
+    print("Request finished!")
